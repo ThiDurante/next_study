8919f7becd5b0b250503ecebdb62c77353c37e14
thiago <thidurante@hotmail.com>
2023-05-02 00:23:48 -0300

    making a request on github api on Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
 
-export default function Home() {
+export default async function Home() {
+  const reponse = await fetch('https://api.github.com/users/thidurante')
+  const data = await reponse.json()
   return (
     <div>
       <h1>Hello World</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
       <Link href="/dashboard">Dashboard</Link>
 
     </div>
